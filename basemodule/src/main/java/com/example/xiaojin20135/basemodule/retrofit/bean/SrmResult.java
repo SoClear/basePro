8e692cbc3ba9082fe85b96c68b3dcee3db820b0f
wangchao <Wchao4@supor.com>
2022-01-24 10:57:28 +0800

    接口返回对象适配

---
+++ basemodule/src/main/java/com/example/xiaojin20135/basemodule/retrofit/bean/SrmResult.java
@@ -0,0 +1,18 @@
+package com.example.xiaojin20135.basemodule.retrofit.bean;
+
+/**
+ * Created by 王超
+ * on 1/24/22
+ */
+class SrmResult {
+    // 角标数量
+    private String total;
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+}
