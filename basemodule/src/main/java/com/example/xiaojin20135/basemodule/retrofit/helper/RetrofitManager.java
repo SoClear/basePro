b10ed655501ba7681ff5efea8d6bc71faf067cd6
LYS <755463684@qq.com>
2022-06-21 11:09:13 +0800

    网络框架添加SSL设置

--- basemodule/src/main/java/com/example/xiaojin20135/basemodule/retrofit/helper/RetrofitManager.java
+++ basemodule/src/main/java/com/example/xiaojin20135/basemodule/retrofit/helper/RetrofitManager.java
@@ -15,7 +15,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.security.KeyStore;
 import java.security.SecureRandom;
+import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
@@ -142,6 +144,7 @@ public enum  RetrofitManager {
         httpClientBuilder.addInterceptor(httpLoggingInterceptor);
         httpClientBuilder.addInterceptor(REWRITE_CACHE_CONTROL_INTERCEPTOR);
         httpClientBuilder.addNetworkInterceptor(REWRITE_CACHE_CONTROL_INTERCEPTOR);
+        httpClientBuilder.sslSocketFactory(createSSLSocketFactory(), new TrustAllCerts());
         httpClientBuilder.hostnameVerifier (new HostnameVerifier () {
             @Override
             public boolean verify (String hostname, SSLSession session) {
@@ -150,8 +153,26 @@ public enum  RetrofitManager {
         });
     }
 
+    private static class TrustAllCerts implements X509TrustManager {
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {}
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {}
+        public X509Certificate[] getAcceptedIssuers() {return new X509Certificate[0];}
+    }
+    private static SSLSocketFactory createSSLSocketFactory() {
+        SSLSocketFactory ssfFactory = null;
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, new TrustManager[]{new TrustAllCerts()}, new SecureRandom());
+            ssfFactory = sc.getSocketFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ssfFactory;
+    }
 
-    //okhttp拦截器
+
+
+        //okhttp拦截器
     private static final Interceptor LoggingInterceptor = new Interceptor() {
         @Override
         public Response intercept(Chain chain) throws IOException {
